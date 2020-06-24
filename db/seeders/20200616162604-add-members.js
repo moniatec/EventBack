24ac03c326c1a2f93ac190591bc1853c7cbadca0
monia <moniatec@gmail.com>
2020-06-24 03:05:57 -0400

    making more seed data

--- db/seeders/20200616162604-add-members.js
+++ db/seeders/20200616162604-add-members.js
@@ -22,7 +22,7 @@ module.exports = {
         },
         {
           userId: "1",
-          eventId: "5",
+          eventId: "3",
           checkedIn: true,
           createdAt: new Date(),
           updatedAt: new Date(),
@@ -182,6 +182,14 @@ module.exports = {
         },
         {
           userId: "4",
+          eventId: "6",
+          checkedIn: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "4",
           eventId: "7",
           checkedIn: false,
           createdAt: new Date(),
@@ -206,7 +214,7 @@ module.exports = {
         },
         {
           userId: "5",
-          eventId: "4",
+          eventId: "5",
           checkedIn: true,
           createdAt: new Date(),
           updatedAt: new Date(),
@@ -280,6 +288,470 @@ module.exports = {
           userId: "6",
           eventId: "8",
           checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "4",
+          eventId: "9",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "1",
+          eventId: "9",
+          checkedIn: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "3",
+          eventId: "9",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "5",
+          eventId: "9",
+          checkedIn: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "4",
+          eventId: "10",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "1",
+          eventId: "10",
+          checkedIn: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "2",
+          eventId: "10",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "3",
+          eventId: "10",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "5",
+          eventId: "10",
+          checkedIn: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "6",
+          eventId: "10",
+          checkedIn: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "4",
+          eventId: "11",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "2",
+          eventId: "11",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "3",
+          eventId: "11",
+          checkedIn: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "5",
+          eventId: "11",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "5",
+          eventId: "12",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "1",
+          eventId: "12",
+          checkedIn: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "2",
+          eventId: "12",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "5",
+          eventId: "13",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "3",
+          eventId: "13",
+          checkedIn: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "6",
+          eventId: "13",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "3",
+          eventId: "14",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "1",
+          eventId: "14",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "2",
+          eventId: "14",
+          checkedIn: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "4",
+          eventId: "14",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "6",
+          eventId: "14",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "3",
+          eventId: "15",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "1",
+          eventId: "15",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "2",
+          eventId: "15",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "4",
+          eventId: "15",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "6",
+          eventId: "15",
+          checkedIn: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "4",
+          eventId: "16",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "1",
+          eventId: "16",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "2",
+          eventId: "16",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "3",
+          eventId: "16",
+          checkedIn: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "5",
+          eventId: "16",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "6",
+          eventId: "16",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "5",
+          eventId: "17",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "2",
+          eventId: "17",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "3",
+          eventId: "17",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "4",
+          eventId: "17",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "6",
+          eventId: "17",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "3",
+          eventId: "18",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "1",
+          eventId: "18",
+          checkedIn: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "2",
+          eventId: "18",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "4",
+          eventId: "18",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "6",
+          eventId: "18",
+          checkedIn: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "6",
+          eventId: "19",
+          checkedIn: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "5",
+          eventId: "19",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "1",
+          eventId: "19",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "2",
+          eventId: "19",
+          checkedIn: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "3",
+          eventId: "19",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "4",
+          eventId: "19",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "1",
+          eventId: "20",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "2",
+          eventId: "20",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "3",
+          eventId: "20",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "4",
+          eventId: "20",
+          checkedIn: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "5",
+          eventId: "20",
+          checkedIn: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+
+        },
+        {
+          userId: "6",
+          eventId: "20",
+          checkedIn: false,
           createdAt: new Date(),
           updatedAt: new Date(),
 
